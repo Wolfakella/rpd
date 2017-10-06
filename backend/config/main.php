189ce795ac77202b8bd68ed007ecbe534e6d201a
Nikolay Olkhovsky <nikolay.olkhovsky@gmail.com>
2017-10-06 21:37:39 +0500

    Initial changes to the project structure implementing the shared hosting configuration.

--- backend/config/main.php
+++ backend/config/main.php
@@ -12,39 +12,30 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
-    'components' => [
-        'request' => [
-            'csrfParam' => '_csrf-backend',
-        ],
-        'user' => [
-            'identityClass' => 'common\models\User',
-            'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
-        ],
-        'session' => [
-            // this is the name of the session cookie used for login on the backend
-            'name' => 'advanced-backend',
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
-    ],
-    'params' => $params,
+	'components' => [
+	    'request' => [
+		'csrfParam' => '_csrf-backend',
+		'csrfCookie' => [
+		    'httpOnly' => true,
+		    'path' => '/admin',
+		],
+	    ],
+	    'user' => [
+		'identityClass' => 'common\models\User',
+		'enableAutoLogin' => true,
+		'identityCookie' => [
+		    'name' => '_identity-backend',
+		    'path' => '/admin',
+		    'httpOnly' => true,
+		],
+	    ],
+	    'session' => [
+		// this is the name of the session cookie used for login on the backend
+		'name' => 'advanced-backend',
+		'cookieParams' => [
+		    'path' => '/admin',
+		],
+	    ],
+	],    
+	'params' => $params,
 ];
